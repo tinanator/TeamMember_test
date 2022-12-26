f26b0b5ad76eb090c924821ee97095405a66d4be
tina <kviditch@gmail.com>
2022-12-26 18:26:16 +0100

    Add navigation and routing

--- TeamMember/ClientApp/src/App.jsx
+++ TeamMember/ClientApp/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
 import './custom.css';
 import Header from './Header';
 import Employees from './Employees';
 import Footer from './Footer';
+import GroupedTeamMembers from './GroupedTeamMembers';
+import Nav from './Nav';
+import NotFound from './NotFound';
 
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
@@ -49,16 +50,25 @@ function App() {
 
 
         return (
-            <div>
+            <Router>
+                <Nav/>
                 <Header SelectedTeam={SelectedTeam}
                     teamNumberCount={employees.filter((employee) => employee.teamName === SelectedTeam).length}
                 />
-                <Employees employees={employees}
-                    SelectedTeam={SelectedTeam}
-                    handleTeamSelectionChange={handleTeamSelectionChange}
-                    handleEmployeeClick={handleEmployeeClick} />
+                <Routes>
+                    <Route path="/"
+                        element={<Employees employees={employees}
+                        SelectedTeam={SelectedTeam}
+                        handleTeamSelectionChange={handleTeamSelectionChange}
+                        handleEmployeeClick={handleEmployeeClick} />}>
+
+                    </Route>
+                    <Route path="/GroupedTeamMembers" element={<GroupedTeamMembers/>}></Route>
+                    <Route path="*" element={<NotFound />}></Route>
+
+                </Routes>
                 <Footer />
-            </div>
+            </Router>
         );
 
 }
